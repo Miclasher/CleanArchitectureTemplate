47ea2ab0a1687fde3a4f987fca7cb85ddbc424cd
Miclasher <14122006mg@gmail.com>
2026-09-24 21:51:58 +0300

    Remove redundant dependancies

--- src/Web.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/Web.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.OpenApi;
-
-namespace Web.Api.Extensions;
+﻿namespace Web.Api.Extensions;
 
 internal static class ServiceCollectionExtensions
 {
@@ -10,26 +7,6 @@ internal static class ServiceCollectionExtensions
         services.AddSwaggerGen(static o =>
         {
             o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
-
-            var securityScheme = new OpenApiSecurityScheme
-            {
-                Name = "JWT Authentication",
-                Description = "Enter your JWT token in this field",
-                In = ParameterLocation.Header,
-                Type = SecuritySchemeType.Http,
-                Scheme = JwtBearerDefaults.AuthenticationScheme,
-                BearerFormat = "JWT"
-            };
-
-            o.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
-
-            o.AddSecurityRequirement(document => new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecuritySchemeReference(JwtBearerDefaults.AuthenticationScheme, document),
-                    []
-                }
-            });
         });
 
         return services;
